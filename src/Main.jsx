c26ddf920ee1913726bfbaa3dde87e33ad4bc647
LuisSalTorro <49000046+LuisSalTorro@users.noreply.github.com>
2022-01-27 17:51:27 -0800

    Remove header on stack navigation

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,23 +1,27 @@
 import 'react-native-gesture-handler';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
 import LandingPage from "./LandingPage/LandingPage"
-import FrontPageStack from "./MainDisplay/FrontPageStack"
+import Drawer from "./MainDisplay/Drawer"
 
-const DrawerNavigator = createDrawerNavigator();
 const Main = () => {
   const Stack = createNativeStackNavigator()
 
+  const mainPageScreenOptions = {
+    headerShown: false
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={mainPageScreenOptions}
+      >
         <Stack.Screen name="LandingPage" component={LandingPage} options={{ title: 'Account'}}/>
-        <Stack.Screen name="FrontPageStack" component={FrontPageStack} />
+        <Stack.Screen name="Drawer" component={Drawer} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- src/MainDisplay/Drawer.jsx
+++ src/MainDisplay/Drawer.jsx
@@ -6,17 +6,19 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import FrontPage from './FrontPage';
 import About from './About';
 
-const FrontPageStack = () => {
+const Drawer = (props) => {
+  const routeParams = props.route.params
+  const username = routeParams.username
   const DrawerNavigator = createDrawerNavigator()
 
   return (
     <NavigationContainer independent={true}>
      <DrawerNavigator.Navigator initialRouteName='FrontPage'>
-        <DrawerNavigator.Screen name='FrontPage' component={FrontPage} />
+        <DrawerNavigator.Screen name='FrontPage' component={FrontPage} initialParams={{username}}/>
         <DrawerNavigator.Screen name='About' component={About} />
-    </DrawerNavigator.Navigator>
+      </DrawerNavigator.Navigator>
     </NavigationContainer>
   )
 }
 
-export default FrontPageStack;
+export default Drawer;
